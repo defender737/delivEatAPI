2f77a00e3c67f2c449bf48349b6206ad2d8f71d1
이신욱 <er888@naver.com>
2023-08-10 12:27:16 +0900

    first commit

---
+++ src/main/java/com/example/delivEatAPI/DelivEatApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.delivEatAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivEatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivEatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/delivEatAPI/DelivEatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.delivEatAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivEatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
