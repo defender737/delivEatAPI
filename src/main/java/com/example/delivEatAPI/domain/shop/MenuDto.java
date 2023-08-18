34bc10cce8d9b6590eb0e05439026450ae0a5d8a
이신욱 <er888@naver.com>
2023-08-19 03:18:06 +0900

    2023-08-19 Controoller,Service,Repository 생성 ERD및 classdiagram 수정

---
+++ src/main/java/com/example/delivEatAPI/domain/shop/MenuDto.java
@@ -0,0 +1,25 @@
+package com.example.delivEatAPI.domain.shop;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MenuDto {
+
+    private Long menuId;
+
+    private String menuName;
+
+    private int menuPrice;
+
+    private String category;
+
+    private Shop shop;
+}
